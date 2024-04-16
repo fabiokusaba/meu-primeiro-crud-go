2d1bba3c5631bc11d6501158a0a79ffb4a5d730b
Fabio Kusaba <fabinhokb44@gmail.com>
2024-04-16 11:51:46 -0300

    criado o construtor do nosso objeto de erros

--- src/configuration/rest_err/rest_err.go
+++ src/configuration/rest_err/rest_err.go
@@ -11,3 +11,12 @@ type Causes struct {
 	Field string `json:"field"`
 	Message string `json:"message"`
 }
+
+func NewRestErr(message, err string, code int, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err: err,
+		Code: code,
+		Causes: causes,
+	}
+}
