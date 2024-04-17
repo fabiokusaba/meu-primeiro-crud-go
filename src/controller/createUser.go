05ed6ab19db74d907fa1d6805d884d930f34306d
Fabio Kusaba <fabinhokb44@gmail.com>
2024-04-17 11:52:29 -0300

    implementado método create user

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,22 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/rest_err"
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/controller/model/request"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
 
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
