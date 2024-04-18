5a61ae1d27f417ca87f825756b2e67fadf6aad31
Fabio Kusaba <fabinhokb44@gmail.com>
2024-04-18 18:54:08 -0300

    refatorado método create, implementado o dominio

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -6,9 +6,13 @@ import (
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/logger"
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/validation"
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/controller/model/request"
-	"github.com/fabiokusaba/meu-primeiro-crud-go/src/controller/model/response"
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/model"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
+)
+
+var (
+	UserDomainInterface model.UserDomainInterface
 )
 
 func CreateUser(c *gin.Context) {
@@ -24,14 +28,14 @@ func CreateUser(c *gin.Context) {
 		return
 	}
 
-	response := response.UserResponse{
-		ID: "test",
-		Email: userRequest.Email,
-		Name: userRequest.Name,
-		Age: userRequest.Age,
+	domain := model.NewUserDomain(userRequest.Email, userRequest.Password, userRequest.Name, userRequest.Age)
+
+	if err := domain.CreateUser(); err != nil {
+		c.JSON(err.Code, err)
+		return
 	}
 
 	logger.Info("User created successfully", zap.String("journey", "createUser"))
 
-	c.JSON(http.StatusOK, response)
+	c.String(http.StatusOK, "")
 }
