789eee225a7acaf840f83f9fbbe92640c0cf42ef
Fabio Kusaba <fabinhokb44@gmail.com>
2024-04-18 19:50:10 -0300

    inicialização das dependências de service e controller

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"log"
 
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/logger"
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/controller"
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/controller/routes"
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/model/service"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -16,10 +18,14 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	//Init dependencies
+	service := service.NewUserDomainService()
+	userController := controller.NewUserControllerInterface(service)
 	
 	router := gin.Default()
 
-	routes.InitRoutes(&router.RouterGroup)
+	routes.InitRoutes(&router.RouterGroup, userController)
 
 	if err := router.Run(":8080"); err != nil {
 		log.Fatal(err)
