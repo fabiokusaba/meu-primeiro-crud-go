d5e6e1570f583c9ffe2b49559f3bb598c365ee84
Fabio Kusaba <fabinhokb44@gmail.com>
2024-04-18 20:11:01 -0300

    inicializando a conexão com o banco de dados

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/database/mongodb"
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/logger"
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/controller"
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/controller/routes"
@@ -18,6 +19,8 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	mongodb.InitConnection()
 
 	//Init dependencies
 	service := service.NewUserDomainService()
