5a0b2b6ab179f999c1fa9633af38c63e961089a8
Fabio Kusaba <fabinhokb44@gmail.com>
2024-04-18 20:28:10 -0300

    refatorado o método de conexão ao banco de dados

--- src/configuration/database/mongodb/mongodb_connection.go
+++ src/configuration/database/mongodb/mongodb_connection.go
@@ -2,22 +2,32 @@ package mongodb
 
 import (
 	"context"
+	"os"
 
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/logger"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func InitConnection() {
-	ctx := context.Background()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+var (
+	MONGODB_URL = "MONGODB_URL"
+	MONGODB_USER_DATABASE = "MONGODB_USER_DATABASE"
+)
+
+func newMongoDBConnection(ctx context.Context) (*mongo.Database, error) {
+	mongodb_uri := os.Getenv(MONGODB_URL)
+	mongodb_database := os.Getenv(MONGODB_USER_DATABASE)
+	
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongodb_uri))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	if err := client.Ping(ctx, nil); err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	logger.Info("MongoDB was connected")
+	
+	return client.Database(mongodb_database), nil
 }
