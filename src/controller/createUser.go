25f920919bf31a576022a6a481e499209122b496
Fabio Kusaba <fabinhokb44@gmail.com>
2024-04-18 07:34:07 -0300

    adicionado validação personalizada no método create user

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -3,8 +3,8 @@ package controller
 import (
 	"fmt"
 	"log"
-
-	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/rest_err"
+	
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/validation"
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
@@ -15,7 +15,7 @@ func CreateUser(c *gin.Context) {
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
 		log.Printf("Error trying to marshal object, error=%s\n", err.Error())
-		errRest := rest_err.NewBadRequestError("Some fields are incorrect")
+		errRest := validation.ValidateUserError(err)
 
 		c.JSON(errRest.Code, errRest)
 		return
