d5a6ef90d1c79bf38e7d96b0eb386aebfabf3249
Fabio Kusaba <fabinhokb44@gmail.com>
2024-04-19 18:42:48 -0300

    refatorado a inicialização das dependências

--- main.go
+++ main.go
@@ -1,31 +1,34 @@
 package main
 
 import (
+	"context"
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/database/mongodb"
 	"log"
 
-	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/database/mongodb"
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/logger"
-	"github.com/fabiokusaba/meu-primeiro-crud-go/src/controller"
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/controller/routes"
-	"github.com/fabiokusaba/meu-primeiro-crud-go/src/model/service"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	logger.Info("About to start user application")
-	
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
+		return
 	}
 
-	mongodb.InitConnection()
+	database, err := mongodb.NewMongoDBConnection(context.Background())
+	if err != nil {
+		log.Fatalf("Error trying to connect to database, error=%s \n", err.Error())
+		return
+	}
 
 	//Init dependencies
-	service := service.NewUserDomainService()
-	userController := controller.NewUserControllerInterface(service)
-	
+	userController := initDependencies(database)
+
 	router := gin.Default()
 
 	routes.InitRoutes(&router.RouterGroup, userController)
