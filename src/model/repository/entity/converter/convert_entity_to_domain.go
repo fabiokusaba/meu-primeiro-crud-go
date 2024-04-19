656f96cc1676af29e98a28b47670db036d0f5ce2
Fabio Kusaba <fabinhokb44@gmail.com>
2024-04-19 18:57:11 -0300

    adicionado os conversores de entity e domain

---
+++ src/model/repository/entity/converter/convert_entity_to_domain.go
@@ -0,0 +1,12 @@
+package converter
+
+import (
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/model"
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/model/repository/entity"
+)
+
+func ConvertEntityToDomain(entity entity.UserEntity) model.UserDomainInterface {
+	domain := model.NewUserDomain(entity.Email, entity.Password, entity.Name, entity.Age)
+	domain.SetID(entity.ID)
+	return domain
+}
