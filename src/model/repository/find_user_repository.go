cf4e397d3eba249a770a34cd2f1043f6affed920
Fabio Kusaba <fabinhokb44@gmail.com>
2024-04-19 20:52:26 -0300

    finalização do método de busca de usuário por id e email

---
+++ src/model/repository/find_user_repository.go
@@ -0,0 +1,76 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/logger"
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/rest_err"
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/model"
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/model/repository/entity"
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/model/repository/entity/converter"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.uber.org/zap"
+	"os"
+)
+
+func (ur *userRepository) FindUserByEmail(email string) (model.UserDomainInterface, *rest_err.RestErr) {
+	logger.Info("Init findUserByEmail repository", zap.String("journey", "findUserByEmail"))
+
+	collection_name := os.Getenv(MONGODB_USER_DATABASE)
+	collection := ur.databaseConnection.Collection(collection_name)
+
+	userEntity := &entity.UserEntity{}
+
+	filter := bson.D{{Key: "email", Value: email}}
+	err := collection.FindOne(context.Background(), filter).Decode(userEntity)
+
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			errorMessage := fmt.Sprintf("User not found with this email: %s", email)
+			logger.Error(errorMessage, err, zap.String("journey", "findUserByEmail"))
+			return nil, rest_err.NewNotFoundError(errorMessage)
+		}
+
+		errorMessage := "Error trying to find user by email"
+		logger.Error(errorMessage, err, zap.String("journey", "findUserByEmail"))
+		return nil, rest_err.NewInternalServerError(errorMessage)
+	}
+
+	logger.Info("FindUserByEmail repository executed successfully",
+		zap.String("journey", "findUserByEmail"),
+		zap.String("email", email),
+		zap.String("userId", userEntity.ID.Hex()))
+	return converter.ConvertEntityToDomain(*userEntity), nil
+}
+
+func (ur *userRepository) FindUserByID(id string) (model.UserDomainInterface, *rest_err.RestErr) {
+	logger.Info("Init findUserByID repository", zap.String("journey", "findUserByID"))
+
+	collection_name := os.Getenv(MONGODB_USER_DATABASE)
+	collection := ur.databaseConnection.Collection(collection_name)
+
+	userEntity := &entity.UserEntity{}
+
+	objectId, _ := primitive.ObjectIDFromHex(id)
+	filter := bson.D{{Key: "_id", Value: objectId}}
+	err := collection.FindOne(context.Background(), filter).Decode(userEntity)
+
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			errorMessage := fmt.Sprintf("User not found with this id: %s", id)
+			logger.Error(errorMessage, err, zap.String("journey", "findUserByID"))
+			return nil, rest_err.NewNotFoundError(errorMessage)
+		}
+
+		errorMessage := "Error trying to find user by id"
+		logger.Error(errorMessage, err, zap.String("journey", "findUserByID"))
+		return nil, rest_err.NewInternalServerError(errorMessage)
+	}
+
+	logger.Info("FindUserByID repository executed successfully",
+		zap.String("journey", "findUserByID"),
+		zap.String("userId", userEntity.ID.Hex()))
+	return converter.ConvertEntityToDomain(*userEntity), nil
+}
--- src/model/service/find_user.go
+++ src/model/service/find_user.go
@@ -1,10 +1,18 @@
 package service
 
 import (
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/logger"
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/rest_err"
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/model"
+	"go.uber.org/zap"
 )
 
-func (*userDomainService) FindUser(string) (*model.UserDomainInterface, *rest_err.RestErr) {
-	return nil, nil	
+func (ud *userDomainService) FindUserByIDServices(id string) (model.UserDomainInterface, *rest_err.RestErr) {
+	logger.Info("Init findUserByID service", zap.String("journey", "findUserByID"))
+	return ud.userRepository.FindUserByID(id)
+}
+
+func (ud *userDomainService) FindUserByEmailServices(email string) (model.UserDomainInterface, *rest_err.RestErr) {
+	logger.Info("Init findUserByEmail service", zap.String("journey", "findUserByEmail"))
+	return ud.userRepository.FindUserByEmail(email)
 }
