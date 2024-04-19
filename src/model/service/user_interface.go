db7ded5d8986d1222a986341f4f0b23a65050ddc
Fabio Kusaba <fabinhokb44@gmail.com>
2024-04-19 11:58:43 -0300

    refatorado o service da aplicação

--- src/model/service/user_interface.go
+++ src/model/service/user_interface.go
@@ -3,14 +3,15 @@ package service
 import (
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/rest_err"
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/model"
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/model/repository"
 )
 
-func NewUserDomainService() UserDomainService {
-	return &userDomainService{}
+func NewUserDomainService(userRepository repository.UserRepository) UserDomainService {
+	return &userDomainService{userRepository}
 }
 
 type userDomainService struct {
-	
+	userRepository repository.UserRepository
 }
 
 type UserDomainService interface {
