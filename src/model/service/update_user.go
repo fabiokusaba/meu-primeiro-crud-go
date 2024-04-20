08d8405189386841e2092087f9a6bba22c733fbe
Fabio Kusaba <fabinhokb44@gmail.com>
2024-04-20 18:53:58 -0300

    implementado update user no service

--- src/model/service/update_user.go
+++ src/model/service/update_user.go
@@ -1,10 +1,22 @@
 package service
 
 import (
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/logger"
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/rest_err"
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/model"
+	"go.uber.org/zap"
 )
 
-func (*userDomainService) UpdateUser(userId string, userDomain model.UserDomainInterface) *rest_err.RestErr {
+func (ud *userDomainService) UpdateUser(userId string, userDomain model.UserDomainInterface) *rest_err.RestErr {
+	logger.Info("Init updateUser model", zap.String("journey", "updateUser"))
+
+	err := ud.userRepository.UpdateUser(userId, userDomain)
+	if err != nil {
+		logger.Error("Error trying to call repository", err, zap.String("journey", "updateUser"))
+		return err
+	}
+
+	logger.Info("UpdateUser service executed successfully", zap.String("userId", userId), zap.String("journey", "updateUser"))
+
 	return nil
 }
