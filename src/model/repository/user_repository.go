53b942f7d2f2fb36642813002db43eee54b71653
Fabio Kusaba <fabinhokb44@gmail.com>
2024-04-20 20:23:34 -0300

    adicionado FindUserByEmailAndPassword na interface de repositorio

--- src/model/repository/user_repository.go
+++ src/model/repository/user_repository.go
@@ -24,6 +24,7 @@ type UserRepository interface {
 	CreateUser(userDomain model.UserDomainInterface) (model.UserDomainInterface, *rest_err.RestErr)
 
 	FindUserByEmail(email string) (model.UserDomainInterface, *rest_err.RestErr)
+	FindUserByEmailAndPassword(email, password string) (model.UserDomainInterface, *rest_err.RestErr)
 	FindUserByID(id string) (model.UserDomainInterface, *rest_err.RestErr)
 
 	UpdateUser(userId string, userDomain model.UserDomainInterface) *rest_err.RestErr
