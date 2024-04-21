d71dc8b01e47aafe35f6b42e01bbf9622d6b86e0
Fabio Kusaba <fabinhokb44@gmail.com>
2024-04-21 10:17:13 -0300

    adicionado middleware de verificação de token nas rotas

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -2,12 +2,13 @@ package routes
 
 import (
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/controller"
+	"github.com/fabiokusaba/meu-primeiro-crud-go/src/model"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes(r *gin.RouterGroup, userController controller.UserControllerInterface) {
-	r.GET("/getUserById/:userId", userController.FindUserByID)
-	r.GET("/getUserByEmail/:userEmail", userController.FindUserByEmail)
+	r.GET("/getUserById/:userId", model.VerifyTokenMiddleware, userController.FindUserByID)
+	r.GET("/getUserByEmail/:userEmail", model.VerifyTokenMiddleware, userController.FindUserByEmail)
 	r.POST("/createUser", userController.CreateUser)
 	r.PUT("/updateUser/:userId", userController.UpdateUser)
 	r.DELETE("/deleteUser/:userId", userController.DeleteUser)
