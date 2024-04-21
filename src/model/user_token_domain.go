79511782b4a20a42c1e2feedb4b163246bd7a339
Fabio Kusaba <fabinhokb44@gmail.com>
2024-04-21 09:43:48 -0300

    implementado os métodos VerifyToken e RemoveBearerPrefix

--- src/model/user_token_domain.go
+++ src/model/user_token_domain.go
@@ -5,6 +5,7 @@ import (
 	"github.com/fabiokusaba/meu-primeiro-crud-go/src/configuration/rest_err"
 	"github.com/golang-jwt/jwt/v5"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -31,4 +32,39 @@ func (ud *userDomain) GenerateToken() (string, *rest_err.RestErr) {
 	}
 
 	return tokenString, nil
+}
+
+func VerifyToken(tokenValue string) (UserDomainInterface, *rest_err.RestErr) {
+	secret := os.Getenv(JWT_SECRET_KEY)
+
+	token, err := jwt.Parse(RemoveBearerPrefix(tokenValue), func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); ok {
+			return []byte(secret), nil
+		}
+
+		return nil, rest_err.NewBadRequestError("Invalid token")
+	})
+
+	if err != nil {
+		return nil, rest_err.NewUnauthorizedRequestError("Invalid token")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return nil, rest_err.NewUnauthorizedRequestError("Invalid token")
+	}
+
+	return &userDomain{
+		id:    claims["id"].(string),
+		email: claims["email"].(string),
+		name:  claims["name"].(string),
+		age:   claims["age"].(int8),
+	}, nil
+}
+
+func RemoveBearerPrefix(token string) string {
+	if strings.HasPrefix(token, "Bearer ") {
+		token = strings.TrimPrefix("Bearer ", token)
+	}
+	return token
 }
